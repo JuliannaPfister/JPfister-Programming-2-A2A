25ab8d5246e4980f5736e7f982a2c7fcaa959658
JuliannaPfister <JuliannaPfister@users.noreply.github.com>
2017-11-10 22:09:25 +1100

    Traps

--- JPfister Game Part A/Assets/Scripts/PlayerMovement.cs
+++ JPfister Game Part A/Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour {
+
+
+public class PlayerMovement : MonoBehaviour
+{
 
     //MOVEMENT 
 
-	// the player speed
-	public float topSpeed;
-	//tells the direction the player is facing
-	bool facingRight = true;
-	//get animator
-	Animator anim;
-	//not grounded
-	bool grounded = false;
-	public Transform groundCheck;
-	float groundRadius = 0.2f;
-	//the jump force
-	public float jumpForce = 700f;
-	// what layer is considered ground
-	public LayerMask whatIsGround;
-	private Rigidbody2D rb;
+    // the player speed
+    public float topSpeed;
+    //tells the direction the player is facing
+    bool facingRight = true;
+    //get animator
+    Animator anim;
+    //not grounded
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    //the jump force
+    public float jumpForce = 700f;
+    // what layer is considered ground
+    public LayerMask whatIsGround;
+    private Rigidbody2D rb;
+    private Collider2D playerCol;
+
+
 
     //COMBAT
 
@@ -33,69 +39,72 @@ public class PlayerMovement : MonoBehaviour {
 
 
 
-	void Start()
-	{
-		anim = GetComponent<Animator>();
-		rb = GetComponent<Rigidbody2D> ();
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        playerCol = GetComponent<Collider2D>();
+
+    }
 
 
-	}
 
 
 
+    void FixedUpdate()
+    {
 
 
-	void FixedUpdate ()
-	{
+        rb.AddForce(Vector2.up * -200f);
+        // if the ground transform hit the whatisground with groundradius
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        //tells the animator that player is grounded
+        anim.SetBool("Ground", grounded);
 
 
-		rb.AddForce(Vector2.up*-200f);
-		// if the ground transform hit the whatisground with groundradius
-		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        //move direction
+        float move = Input.GetAxis("Horizontal");
 
-		//tells the animator that player is grounded
-		anim.SetBool("Ground", grounded);
+        //adds velocity to the rigidbody in the move direction * speed
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        // gets how fast we move up or down from the rigidbody
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        // if player is moving left calls the function Flip() if it moves right call function Flip() again
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+
+    }
+
+    void Update()
+    {
+        //if player is on the ground and space key is pressed
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {   //Set isgrounded to false
+            anim.SetBool("Ground", false);
+            //and add jump force
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
 
 
-		//move direction
-		float move = Input.GetAxis("Horizontal");
+    }
 
-		//adds velocity to the rigidbody in the move direction * speed
-		GetComponent<Rigidbody2D>().velocity = new Vector2(move*topSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		// gets how fast we move up or down from the rigidbody
-		anim.SetFloat ("Speed", Mathf.Abs (move));
+    //the function Flip transforms the scale to flip the player object left or right
+    void Flip()
+    {
+        facingRight = !facingRight;
+        //get local scale
+        Vector3 theScale = transform.localScale;
+        //flips on x axis
+        theScale.x *= -1;
+        //appy to local scale
+        transform.localScale = theScale;
+    }
 
-		// if player is moving left calls the function Flip() if it moves right call function Flip() again
-		if (move > 0 && !facingRight)
-			Flip ();
-		else if (move < 0 && facingRight)
-			Flip ();
-
-	}
-
-	void Update()
-	{
-		if (grounded && Input.GetKeyDown(KeyCode.Space))
-		{
-			anim.SetBool("Ground", false);
-			//add jump force
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
-		}
-	}
-
-	//the function Flip transforms the scale to flip the player object left or right
-	void Flip ()
-	{
-		facingRight = !facingRight;
-		//get local scale
-		Vector3 theScale = transform.localScale;
-		//flips on x axis
-		theScale.x *= -1;
-		//appy to local scale
-		transform.localScale = theScale;
-	}
-
-    void Hurt()
+    public void Hurt()
     {
         // if health is lower than zero, restart game
         health--;
@@ -116,30 +125,40 @@ public class PlayerMovement : MonoBehaviour {
         //ignore collision with enemies
         int enemyLayer = LayerMask.NameToLayer("Enemy");
         int playerLayer = LayerMask.NameToLayer("Player");
-        
 
-        Physics2D.IgnoreLayerCollision(enemyLayer,playerLayer);
+
+
+        Physics2D.IgnoreLayerCollision(enemyLayer, playerLayer);
         //start looping blinking anim
         anim.SetLayerWeight(1, 1);
 
         //wait for invincibility to end
         yield return new WaitForSeconds(hurtTime);
         //stops blinking animation and re enable collision
-        Physics2D.IgnoreLayerCollision(enemyLayer, playerLayer,false);
+        Physics2D.IgnoreLayerCollision(enemyLayer, playerLayer, false);
         anim.SetLayerWeight(1, 0);
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+
+    public void OnCollisionEnter2D(Collision2D collision)
     {
         //if player collides with enemy, calls function Hurt()
         Enemy enemy = collision.collider.GetComponent<Enemy>();
         if (enemy != null)
+
+
+        { 
+        Hurt();
+        }
+
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Traps"))
         {
             Hurt();
         }
     }
-    
-        
+
    
 }
