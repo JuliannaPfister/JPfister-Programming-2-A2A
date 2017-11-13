895cfd6b9ebd50c3099328a9a9e0fce95d74dfdc
JuliannaPfister <JuliannaPfister@users.noreply.github.com>
2017-11-13 22:22:32 +1100

    Update 5.2

---
+++ JPfister Game Part A/Assets/Scripts/WallJump.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallJump : MonoBehaviour {
+    public float distance=1f;
+    public float speed = 2f;
+    PlayerMovement movement;
+
+	// Use this for initialization
+	void Start () {
+
+        movement = GetComponent<PlayerMovement>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Physics2D.raycastsStartInColliders = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !movement.groundCheck && hit.collider!=null)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed*hit.normal.x, speed);
+        }
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
+    }
+}
